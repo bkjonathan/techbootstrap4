d24ec34292f4622e9ba343f513b6aed14fd959f2
bkjonathan <admin@sinphyukyun.com>
2017-11-21 07:20:47 +0630

    Add all tasks and finished files

---
+++ template/foot.php
@@ -0,0 +1,7 @@
+
+<script src="js/jquery.slim.js"></script>
+<script src="js/tether.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
