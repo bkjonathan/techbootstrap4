c7c7673182df1f9dab5ef4d5b753aa115ade6eee
Kyaw Zin <immortalityskill2398@gmail.com>
2017-11-23 16:49:48 +0630

    nav complete

--- index.php
+++ index.php
@@ -18,8 +18,31 @@ require_once 'template/head.php';
 
         </div>
     </div>
-
 </header>
+<div class="navigation py-2 mt-4">
+    <nav class="main-nav py-2">
+        <div class="container">
+            <ul class="nav nav-justified flex-column flex-sm-row">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
 
 <?php
 require_once 'template/foot.php';
