aa685b3241e1dbba5ad85b80bc76513161e2eb88
bkjonathan <admin@sinphyukyun.com>
2017-11-23 14:41:29 +0630

    Ep-2

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'template/head.php';
+?>
+<header class="site-header container">
+    <div class="row justify-content-between">
+        <div class="col-8 offset-2 col-lg-4 offset-lg-0">
+            <img src="assets/img/logo.svg" class="img-fluid mx-auto d-block">
+        </div>
+        <div class="col-12 col-lg-4">
+            <nav class="socials">
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">Instagram</a>
+                <a href="#">Pinterest</a>
+                <a href="#">Youtube</a>
+
+            </nav>
+
+        </div>
+    </div>
+
+</header>
+
+<?php
+require_once 'template/foot.php';
+?>
