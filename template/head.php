4ec6bc46e9bb284590fec77cbfb93d3e28453508
Kyaw Zin <immortalityskill2398@gmail.com>
2017-11-23 18:57:43 +0630

    Add slideshow and text section

--- template/head.php
+++ template/head.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Lato|Pacifico" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Mukta+Malar|Pacifico" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Mukta+Malar|Pacifico" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
