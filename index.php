4833257ab3a267766082a4410d4c38d26d63354d
Kyaw Zin <immortalityskill2398@gmail.com>
2017-11-26 22:22:18 +0630

    Finish index page

--- index.php
+++ index.php
@@ -1,25 +1,29 @@
 <?php
 require_once 'template/head.php';
 ?>
+<!-- Header -->
 <header class="site-header container">
     <div class="row justify-content-between">
         <div class="col-8 offset-2 col-lg-4 offset-lg-0">
             <img src="assets/img/logo.svg" class="img-fluid mx-auto d-block">
         </div>
         <div class="col-12 col-lg-4">
-            <nav class="socials">
-                <a href="#">Facebook</a>
-                <a href="#">Twitters</a>
-                <a href="#">Instagram</a>
-                <a href="#">Pinterest</a>
-                <a href="#">Youtube</a>
-
+            <nav class="socials text-center text-md-right pt-5">
+                <ul>
+                    <li><a href="#"><i class="fa fa-2x fa-facebook-official" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-2x fa-twitter-square" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-2x fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-2x fa-pinterest-square" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-2x fa-youtube-square" aria-hidden="true"></i></a></li>
+                </ul>
             </nav>
 
         </div>
     </div>
 </header>
-<div class="navigation py-2 mt-4">
+
+<!-- Navigation -->
+<div class="pink-cyan-gradient  py-2 mt-4">
     <nav class="main-nav py-2">
         <div class="container">
             <ul class="nav nav-justified flex-column flex-sm-row">
@@ -42,44 +46,32 @@ require_once 'template/head.php';
         </div>
     </nav>
 </div>
+
+<!-- Carousel -->
 <main class="container">
     <div id="main-slider" data-ride="carousel" class="carousel slide mt-4">
         <ul class="carousel-indicators">
             <li data-target="#main-slider" data-slide-to="0" class="active"></li>
             <li data-target="#main-slider" data-slide-to="1"></li>
             <li data-target="#main-slider" data-slide-to="2"></li>
-            <li data-target="#main-slider" data-slide-to="3"></li>
-            <li data-target="#main-slider" data-slide-to="4"></li>
         </ul>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="assets/img/slide_01.jpg" class="d-block img-fluid img-thumbnail">
+                <img src="assets/img/img_two/bg1.jpg" class="d-block img-fluid img-thumbnail">
                 <div class="carousel-caption d-none d-md-block">
-                    <h1 class="text-primary text-uppercase">Mogok</h1>
+                    <h2 class="text-primary text-uppercase">Mogok</h2>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="assets/img/slide_02.jpg" class="d-block img-fluid img-thumbnail">
+                <img src="assets/img/img_two/bg2.jpg" class="d-block img-fluid img-thumbnail">
                 <div class="carousel-caption d-none d-md-block">
-                    <h1 class="text-primary text-uppercase">Mandalay</h1>
+                    <h2 class="text-primary text-uppercase">Mandalay</h2>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="assets/img/slide_03.jpg" class="d-block img-fluid img-thumbnail">
+                <img src="assets/img/img_two/bg3.jpg" class="d-block img-fluid img-thumbnail">
                 <div class="carousel-caption d-none d-md-block">
-                    <h1 class="text-primary text-uppercase">MogokMandalay</h1>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="assets/img/slide_04.jpg" class="d-block img-fluid img-thumbnail">
-                <div class="carousel-caption d-none d-md-block">
-                    <h1 class="text-primary text-uppercase">Yangon</h1>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="assets/img/slide_05.jpg" class="d-block img-fluid img-thumbnail">
-                <div class="carousel-caption d-none d-md-block">
-                    <h1 class="text-primary text-uppercase">NaypyiTaw</h1>
+                    <h2 class="text-primary text-uppercase">MogokMandalay</h2>
                 </div>
             </div>
         </div>
@@ -93,11 +85,237 @@ require_once 'template/head.php';
         </a>
     </div>
 </main>
+
+<!-- New Website section-->
 <section class="new-website py-5">
     <h2 class="text-center text-uppercase"><span class="text-lowercase my-4">Welcome to our new</span> Website</h2>
     <p class="text-center">My name is Kyaw Zin Htike.I live in <br>Mandalay</p>
 </section>
 
+<!-- Image Links -->
+<div class="container pb-5 mt-4">
+    <div class="row">
+        <div class="col-md-4 col-12 text-center mb-4 mb-md-0">
+            <div class="image-links">
+                <img class="img-fluid img-thumbnail" src="assets/img/service_01.jpg">
+            </div>
+            <div class="row no-gutters">
+                <div class="col-8 offset-2 col-md-10 offset-md-1 image-info pt-3">
+                    <h3 class="text-center text-uppercase"><span class="text-lowercase">Learn More</span> About Us</h3>
+                    <a href="#" class="btn btn-primary text-uppercase btn-block py-3 mt-4">read more</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-12 text-center mb-4 mb-md-0">
+            <div class="image-links">
+                <img class="img-fluid img-thumbnail" src="assets/img/service_02.jpg">
+            </div>
+            <div class="row no-gutters">
+                <div class="col-8 offset-2 col-md-10 offset-md-1 image-info pt-3">
+                    <h3 class="text-center text-uppercase"><span class="text-lowercase">about our</span> Services</h3>
+                    <a href="#" class="btn btn-primary text-uppercase btn-block py-3 mt-4">read more</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-12 text-center mb-4 mb-md-0">
+            <div class="image-links">
+                <img class="img-fluid img-thumbnail" src="assets/img/service_03.jpg">
+            </div>
+            <div class="row no-gutters">
+                <div class="col-8 offset-2 col-md-10 offset-md-1 image-info pt-3">
+                    <h3 class="text-center text-uppercase"><span class="text-lowercase">visit our</span> Store</h3>
+                    <a href="#" class="btn btn-primary text-uppercase btn-block py-3 mt-4">read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Business Hours-->
+<div class="business-hours pink-cyan-gradient py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center">
+                <h3 class="text-uppercase">Business Hours</h3>
+                <p class="mt-5">The carousel is a slideshow for cycling through a series of content, built with CSS 3D
+                    transforms and
+                    a bit of JavaScript.</p>
+                <table class="table table-hover text-center">
+                    <thead class="table-danger">
+                    <tr>
+                        <th class="text-center">Day</th>
+                        <th class="text-center">From</th>
+                        <th class="text-center">To</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Monday</td>
+                        <td>09:00</td>
+                        <td>19:00</td>
+                    </tr>
+                    <tr>
+                        <td>Tuesday</td>
+                        <td>09:00</td>
+                        <td>19:00</td>
+                    </tr>
+                    <tr>
+                        <td>Wednesday</td>
+                        <td>09:00</td>
+                        <td>19:00</td>
+                    </tr>
+                    <tr>
+                        <td>Thursday</td>
+                        <td>09:00</td>
+                        <td>19:00</td>
+                    </tr>
+                    <tr>
+                        <td>Friday</td>
+                        <td>09:00</td>
+                        <td>19:00</td>
+                    </tr>
+                    <tr>
+                        <td>Saturday</td>
+                        <td>09:00</td>
+                        <td>19:00</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday</td>
+                        <td>Closed</td>
+                        <td>Closed</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6 bg-hours">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Product Section -->
+<div class="container products py-5">
+    <h3 class="text-center text-uppercase"><span class="text-lowercase">check out</span> Our Products</h3>
+    <div class="row mt-5">
+        <div class="col-6 col-md-3 mb-5 mb-md-0">
+            <div class="card">
+                <a href="#">
+                    <img src="assets/img/product_thumb_01.jpg" class="card-img-top img-fluid">
+                    <div class="card-block">
+                        <h3 class="card-title text-uppercase text-center mb-0">Product1</h3>
+                        <div class="card-text">
+                            <p>
+                                The carousel is a slideshow for cycling through a series of content
+                            </p>
+                            <p class="price text-center mb-0">$ 25</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-md-3 mb-5 mb-md-0">
+            <div class="card">
+                <a href="#">
+                    <img src="assets/img/product_thumb_02.jpg" class="card-img-top img-fluid">
+                    <div class="card-block">
+                        <h3 class="card-title text-uppercase text-center mb-0">Product1</h3>
+                        <div class="card-text">
+                            <p>
+                                The carousel is a slideshow for cycling through a series of content
+                            </p>
+                            <p class="price text-center mb-0">$ 25</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-md-3 mb-5 mb-md-0">
+            <div class="card">
+                <a href="#">
+                    <img src="assets/img/product_thumb_03.jpg" class="card-img-top img-fluid">
+                    <div class="card-block">
+                        <h3 class="card-title text-uppercase text-center mb-0">Product1</h3>
+                        <div class="card-text">
+                            <p>
+                                The carousel is a slideshow for cycling through a series of content
+                            </p>
+                            <p class="price text-center mb-0">$ 25</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-md-3 mb-5 mb-md-0">
+            <div class="card">
+                <a href="#">
+                    <img src="assets/img/product_thumb_04.jpg" class="card-img-top img-fluid">
+                    <div class="card-block">
+                        <h3 class="card-title text-uppercase text-center mb-0">Product1</h3>
+                        <div class="card-text">
+                            <p>
+                                The carousel is a slideshow for cycling through a series of content
+                            </p>
+                            <p class="price text-center mb-0">$ 25</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Appointment -->
+<div class="appointment container-fluid py-5">
+    <div class="row">
+        <div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 text-center">
+            <h3 class="text-uppercase text-center">Make an appointment</h3>
+            <p>The carousel is a slideshow for cycling through a series of content, built with CSS 3D transforms and a
+                bit of JavaScript.</p>
+            <a href="#" class="btn btn-primary btn-lg text-uppercase">Read More</a>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="site-footer pt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="text-center text-uppercase pb-4">About us</h3>
+                <p class="text-justify">The carousel is a slideshow for cycling through a series of content, built with
+                    CSS 3D transforms and a bit of JavaScript.</p>
+            </div>
+            <div class="col-md-4 pb-4 pb-md-0">
+                <h3 class="text-center text-uppercase pb-4">Open Hours</h3>
+                <p class="text-center mb-0">Mon - Fri : 9 AM - 7 PM</p>
+                <p class="text-center mb-0">Saturday : 10 AM - 2 PM</p>
+                <p class="text-center mb-0">Sunday : Closed</p>
+            </div>
+            <div class="col-md-4">
+                <h3 class=" text-uppercase text-center pb-4">Contacts</h3>
+                <p class="mb-0">66 East Sunnyslope Avenue</p>
+                <p>Lansdowne,PA 19050</p>
+                <div class="social-nav">
+                    <nav class="socials text-center">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-official" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-2x fa-instagram" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-2x fa-pinterest-square" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-2x fa-youtube-square" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="w-100"></div>
+            <hr class="w-100">
+            <p class="copyright text-center w-100">
+                Carolina Spa & Salon Copyright
+            </p>
+        </div>
+    </div>
+</footer>
 <?php
 require_once 'template/foot.php';
 ?>
--- template/head.php
+++ template/head.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Lato|Pacifico" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato|Mukta+Malar|Pacifico" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato|Mukta+Malar|Pacifico" rel="stylesheet">
