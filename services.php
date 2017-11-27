80fb05cdb3b1bddcb865483a30cd1f60deaec595
Kyaw Zin <immortalityskill2398@gmail.com>
2017-11-27 21:13:47 +0630

    modified nav for moblile

--- services.php
+++ services.php
@@ -3,6 +3,9 @@ require_once 'template/head.php';
 require_once 'template/nav.php';
 ?>
 
+<div class="container py-5">
+    <h2 class="text-center">Great</h2>
+</div>
 
 <?php
 require_once 'template/foot.php';
